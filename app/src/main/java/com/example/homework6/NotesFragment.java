57fbf4dad7d653afb76b52c6e90eb767be6eb9a0
Akimov <akimov1986@internet.ru>
2021-08-10 20:16:54 +0700

    Создал контейнеры для фрагментов.

---
+++ app/src/main/java/com/example/homework6/NotesFragment.java
@@ -0,0 +1,19 @@
+package com.example.homework6;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class NotesFragment extends Fragment {
+    public static NotesFragment newInstance() {
+        return new NotesFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
