a902278e5ab433461b1db26afb9412213197335b
Akimov <akimov1986@internet.ru>
2021-08-11 20:21:49 +0700

    Создал каркас(один фрагмент в портретной,два фрагмента в ландшафтной ориентации).

--- app/src/main/java/com/example/homework6/DescriptionOfNotesFragment.java
+++ app/src/main/java/com/example/homework6/DescriptionOfNotesFragment.java
@@ -5,16 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class NotesFragment extends Fragment {
-    public static NotesFragment newInstance() {
-        return new NotesFragment();
+public class DescriptionOfNotesFragment extends Fragment {
+    public static DescriptionOfNotesFragment newInstance() {
+        return new DescriptionOfNotesFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_notes, container,false);
+        View view = inflater.inflate(R.layout.fragment_description_of_notes, container, false);
         return view;
     }
 }
--- app/src/main/java/com/example/homework6/MainActivity.java
+++ app/src/main/java/com/example/homework6/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.homework6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.notes_container, NotesFragment.newInstance())
+                .commit();
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.characteristic_notes_container, DescriptionOfNotesFragment.newInstance())
+                    .commit();
+        }
     }
 }
